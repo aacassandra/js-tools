877f5717f81d1d4cf1fb1d6d38cee7511a57d73f
aacassandra <alauddinafifcassandra@programmer.net>
2019-09-15 18:42:35 +0700

    Support sort of nested properti object

--- src/tools.js
+++ src/tools.js
@@ -228,6 +228,27 @@ export let JsLib = {
           var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
           return result * sortOrder;
         }
+      },
+      nestedProperty(prop, arr) {
+        prop = prop.split('.');
+        var len = prop.length;
+
+        arr.sort(function(a, b) {
+          var i = 0;
+          while (i < len) {
+            a = a[prop[i]];
+            b = b[prop[i]];
+            i++;
+          }
+          if (a < b) {
+            return -1;
+          } else if (a > b) {
+            return 1;
+          } else {
+            return 0;
+          }
+        });
+        return arr;
       }
     }
   },
